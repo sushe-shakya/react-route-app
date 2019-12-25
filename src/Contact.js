e087fcac60226393d6c87dc40c6456b29e1fb437
Sushil Shakya <sushil.shakya@fusemachines.com>
2019-12-25 12:21:59 +0545

    Setup react router basic application

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+  render(){
+    return(
+      <div>
+        I am Contact.
+      </div>
+    );
+  }
+}
+
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from './Home';
+import Contact from './Contact';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router>
+  <div>
+    <ul>
+      <li>
+        <Link to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/contact">
+          Contact-me
+        </Link>
+      </li>
+    </ul>
+    <hr/>
+
+    <Route exact path="/" component={Home}/>
+    <Route path="/contact" component={Contact}/>
+
+  </div>
+
+        </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
